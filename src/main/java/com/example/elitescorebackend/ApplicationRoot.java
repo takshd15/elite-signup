ea2375c8c0d1b16742a1e1a74cccb9f1f8a02ae2
bossNilac <calin.baculescu@gmail.com>
2025-08-07 20:56:30 +0300

    Deployment fix2

--- src/main/java/com/example/elitescorebackend/ApplicationRoot.java
+++ src/main/java/com/example/elitescorebackend/ApplicationRoot.java
@@ -1,9 +1,16 @@
 package com.example.elitescorebackend;
 
+import com.example.elitescorebackend.util.AuthCORSFilter;
 import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
+import org.glassfish.jersey.server.ResourceConfig;
 
 @ApplicationPath("/v1")
-public class ApplicationRoot extends Application {
-
-}
+    public class ApplicationRoot extends ResourceConfig {
+        public ApplicationRoot() {
+            // scan all your resource packages
+            packages("com.example.elitescorebackend");
+            // register any custom providers/filters explicitly
+            register(com.example.elitescorebackend.util.JacksonConfig.class);
+            register(AuthCORSFilter.class);
+        }
+    }
